f034ee3855bb341cba425f453e357b6b909ee408
balabrasil <107439627+balabrasil@users.noreply.github.com>
2022-06-26 13:29:32 -0400

    Delete EnvironmentPath.cmd

--- EnvironmentPath.cmd
+++
@@ -1,9 +0,0 @@
-@echo OFF
-
-:: Setting Toolkit environment path variables
-rem %~dp0 value for WorkingFolder is Toolkit's root folder.
-set "WorkingFolder=%~dp0"
-rem Here is for Personal folder.
-rem set "WorkingFolder=%SystemDRIVE%\TK\"
-set "ISOsource=%~dp0ISOsource"
-set "ISO=%~dp0ISO"
