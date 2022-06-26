4bf1089dad2b4f739f94ff599f268a94bd1dc23b
balabrasil <107439627+balabrasil@users.noreply.github.com>
2022-06-26 17:36:17 -0400

    Update EnvironmentPath.cmd

--- EnvironmentPath.cmd
+++ EnvironmentPath.cmd
@@ -1,7 +1,7 @@
 @echo OFF
 
 :: Setting Toolkit environment path variables
-rem %~dp0 value for WorkingFolder is Toolkit's root folder.
+rem The "ROOT=%~dp0 (var=value)" for "WorkingFolder var" is the Toolkit's ROOT folder.
 set "WorkingFolder=%ROOT%"
 rem Here is for Personal folder.
 rem set "WorkingFolder=%SystemDRIVE%\TK\"
