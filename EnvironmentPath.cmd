0a4b43f87343156221016ac6aa3052a09b7b05a3
balabrasil <107439627+balabrasil@users.noreply.github.com>
2022-06-28 20:10:32 -0400

    Update EnvironmentPath.cmd

--- EnvironmentPath.cmd
+++ EnvironmentPath.cmd
@@ -2,8 +2,10 @@
 
 :: Setting Toolkit environment path variables
 rem The "ROOT=%~dp0 (var=value)" for "WorkingFolder var" is the Toolkit's ROOT folder.
+rem Note 1: If the WorkingFolder variable remains empty, this will be automatically defined as Toolkit root folder (set "WorkingFolder=%ROOT%").
 set "WorkingFolder=%ROOT%"
+
 rem Here is for Personal folder.
-rem set "WorkingFolder=%SystemDRIVE%\TK\"
-set "ISOsource=%ROOT%ISOsource"
-set "ISO=%ROOT%ISO"
+rem Note 2: If you put a custom path (bitwin = and "), for the "WorkingFolder" variable, the new path will need to end with "\". It is CRUCIAL (Very Important).
+rem Example: set "WorkingFolder=%SystemDRIVE%\TK\"
+rem set "WorkingFolder="
